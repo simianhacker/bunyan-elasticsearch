0dd6bc5f7f98b00714c331e5665cc8a5ad48e482
Chris Cowan <chriscowan@plus3network.com>
2014-02-17 15:52:07 -0700

    Removing some opinions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Writable = require('stream').Writable;
 var domain = require('domain');
 var util = require('util');
-var es = require('elasticserch');
+var elasticsearch = require('elasticsearch');
 var moment = require('moment');
 
 var levels = {
@@ -13,52 +13,61 @@ var levels = {
   60: 'fatal'
 };
 
+function generateIndexName (entry) {
+  var datestamp = moment(entry.timestamp).format('YYYY.MM.DD');
+  return 'logstash-'+datestamp;
+}
+
+function callOrString (value, entry) {
+  if (typeof(value) === 'function') {
+    return value(entry);
+  }
+  return value;
+}
+
 function ElasticsearchStream (options) {
+  this.client = new elasticsearch.Client(options);
+  this.type = options.type || 'logs';
+  this.index = options.index || generateIndexName; 
   Writable.call(this, options);
 }
 
 util.inherits(KibanaStream, Writable);
 
 KibanaStream.prototype._write = function (entry, encoding, callback) {
+
+  var client = this.client;
+  var index = this.index;
+  var type = this.type;
+
   var d = domain.create();
   d.on('error', function (err) { 
-    console.log("KibanaStream Error", err.stack);
+    console.log("Elasticsearch Error", err.stack);
   });
   d.run(function () {
     entry = JSON.parse(entry.toString('utf8'));
-
     var env = process.env.NODE_ENV || 'development';
 
-    var data = {
-      '@timestamp': entry.time,
-      'timestamp': entry.time,
-      message: entry.msg,
-      tags: [levels[entry.level]],
-      name: entry.name,
-      level: levels[entry.level],
-      hostname: entry.hostname,
-      pid: entry.pid,
-      '@version': "1"
-    }
+    // Reassign these fields so them match what the default Kibana dashboard 
+    // expects to see.
+    entry['@timestamp'] = entry.time;
+    entry.level = levels[entry.level];
+    entry.message = entry.msg;
 
-    if (entry.req) data.req = entry.req;
-    if (entry.req) data.res = entry.res;
-    if (entry.err) data.err = entry.err;
+    // remove duplicate fields
+    delete entry.time;
+    delete entry.msg;
 
     var datestamp = moment(entry.timestamp).format('YYYY.MM.DD');
+
     var options = {
-      url: config.elasticsearch.url.replace(/[^\/]+$/, 'logstash-'+datestamp+'/logs'),
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body:  JSON.stringify(data)
+      index: callOrString(index, entry),
+      type: callOrString(type, entry),
+      body: entry;
     };
 
-    request(options, function (err, resp, body) {
-      if (err) console.log('KibanaStream Error Request Error', err.stack); // We need to ignore errors.
-      callback();
-    });
+    client.create(options, callback);
+
   });
 };
 
