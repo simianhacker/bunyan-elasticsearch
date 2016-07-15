8910d5adc712417370a798eb554483256a2fa004
Alexandre <alexandre@lulja.com>
2016-07-15 14:50:54 +0200

    Update dependencies, add travis test build

---
+++ tests/connection.js
@@ -0,0 +1,16 @@
+'use-strict';
+
+var requireNew = require('require-new'),
+  should = require('should');
+
+describe('Connection', function () {
+
+  describe('Test build', function () {
+
+    it('test', function () {
+      should.equal(1, 1);
+    });
+
+  });
+
+});
